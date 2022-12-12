8a527c222983d09c7e56c3888c3d48319456d6d1
Shephard-S <samtechcyberlinkers@gmail.com>
2022-12-12 07:48:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*Printing using puts
+/*
+ * Printing using puts
  */
 
 int main(void)
 
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	rerturn (0);
