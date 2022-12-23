f3c19bc40c0e28b04b4a2d851ee2ed1108b54c04
Shephard-S <samtechcyberlinkers@gmail.com>
2022-12-23 00:28:19 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ *
+ * main -Entry point
+ *
+ * return (0) always success
+ *
+ * */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
